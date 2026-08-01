00aa1f959d0c228ba57451b5cf1e3ef9ad7494af
KhaiWah <khaiwah@nebraska.edu>
2026-07-31 23:05:37 -0500

    Update entrypoint.sh
    updated the entrypoint

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -6,14 +6,12 @@ export STATE_KEY=${INPUT_STATE_KEY}
 export TF_STAGE=$(echo "${INPUT_TF_STAGE}" | xargs) 
 export DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}
 
-
-
 if [[ "$TF_STAGE" == "stage1" ]]; then
     terraform -chdir=${INPUT_TF_STAGE} init -backend-config="prefix=terraform/${INPUT_STATE_KEY}/stage1"
     terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
     terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
 
-    elif [[ "$TF_STAGE" == "stage2" ]]; then
+elif [[ "$TF_STAGE" == "stage2" ]]; then
     terraform -chdir=${INPUT_TF_STAGE} init -backend-config="prefix=terraform/${INPUT_STATE_KEY}/stage2"
     terraform -chdir=${INPUT_TF_STAGE} apply -auto-approve -var="DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}"
 
