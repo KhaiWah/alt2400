6a2993a74c0c10d4e2e704e0ddd746fa8a4ed9d3
KhaiWah <khaiwah@nebraska.edu>
2026-07-31 22:53:05 -0500

    Update entrypoint.sh
    update to retain our stages 2&3

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -12,4 +12,13 @@ if [[ "$TF_STAGE" == "stage1" ]]; then
     terraform -chdir=${INPUT_TF_STAGE} init -backend-config="prefix=terraform/${INPUT_STATE_KEY}/stage1"
     terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
     terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+
+    elif [[ "$TF_STAGE" == "stage2" ]]; then
+    terraform -chdir=${INPUT_TF_STAGE} init -backend-config="prefix=terraform/${INPUT_STATE_KEY}/stage2"
+    terraform -chdir=${INPUT_TF_STAGE} apply -auto-approve -var="DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}"
+
+elif [[ "$TF_STAGE" == "stage3" ]]; then
+    terraform -chdir=${INPUT_TF_STAGE} init -backend-config="prefix=terraform/${INPUT_STATE_KEY}/stage3"
+    terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
+    terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
 fi
