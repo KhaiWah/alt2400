481fab9daca8b941272183c097582f0a836c6f4d
KhaiWah <khaiwah@nebraska.edu>
2026-07-31 22:53:51 -0500

    Update main.tf

--- stage3/main.tf
+++ stage3/main.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
 
+  backend "gcs" {
+    bucket = "khai_wah_bucket"
+    prefix = "terraform/stage3"
+  }
+}
+
+provider "google" {
+  project = "devopalt"
+  region  = "us-east1"
+}
