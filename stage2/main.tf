833fd0052ce5e016105798a543c45affc75b1ff5
KhaiWah <khaiwah@nebraska.edu>
2026-07-31 22:36:17 -0500

    Create main.tf

---
+++ stage2/main.tf
@@ -0,0 +1,60 @@
+variable "DJANGO_SECRET_KEY_PROD" {}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "khai_wah_bucket"
+    prefix = "terraform/stage2"
+  }
+}
+
+provider "google" {
+  project = "devopalt"
+  region  = "us-east1"
+}
+
+resource "google_cloud_run_v2_service" "khaiwah-cloudrun" {
+  name     = "acmp-khaiwah-instance"
+  location = "us-east1"
+
+  template {
+    containers {
+      name  = "final-app"
+      image = "us-east1-docker.pkg.dev/devopalt/khaiwah-repo/final:latest"
+
+      ports {
+        container_port = 8000
+      }
+
+      env {
+        name  = "DJANGO_SECRET_KEY"
+        value = var.DJANGO_SECRET_KEY_PROD
+      }
+
+      resources {
+        limits = {
+          cpu    = "1"  
+          memory = "512Mi" 
+        }
+      }
+    }
+  }
+
+  traffic {
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_member" "public_access" {
+  name     = google_cloud_run_v2_service.khaiwah-cloudrun.name
+  location = google_cloud_run_v2_service.khaiwah-cloudrun.location
+  role     = "roles/run.viewer"
+  member   = "allUsers"
+}
